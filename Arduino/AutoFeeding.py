2eb45eba316268fada121c946bc8df4bc207b9c2
JongLee27 <144095568+JongLee27@users.noreply.github.com>
2023-11-20 14:43:36 +0900

    Update AutoFeeding.py

--- Arduino/AutoFeeding.py
+++ Arduino/AutoFeeding.py
@@ -1,16 +1,16 @@
 #include <Servo.h>
 
-const int ServoPin = 10;
-const int LittlePin = 5;
+const int ServoPin = 10; //서보모터 핀
+const int LittlePin = 5; //버튼 핀
 const int HalfPin = 6;
 const int FullPin = 7;
-int Pos = 0;
+int Pos = 0; //서보모터 각도 설정
 
 Servo myservo;
 
 void setup() {
   myservo.attach(ServoPin);
-  myservo.write(0);
+  myservo.write(Pos);
   
   pinMode(LittlePin, INPUT);
   pinMode(HalfPin, INPUT);
@@ -18,11 +18,11 @@ void setup() {
 }
 
 void loop() {
-  if(digitalRead(LittlePin) == HIGH){
-    Pos = 30;
-    myservo.write(Pos);
-    delay(1000);
-    Pos = 0;
+  if(digitalRead(LittlePin) == HIGH){ //버튼이 눌렸는지 확인
+    Pos = 30; //서보모터 각도 지정
+    myservo.write(Pos); //서보모터 제어
+    delay(1000); 
+    Pos = 0; //서보모터 각도 초기값으로 지정 
     myservo.write(Pos);
   }
   else if(digitalRead(HalfPin) == HIGH) {
