eff13e88917c4ce8572797abca743620cf3b4dbb
JongLee27 <144095568+JongLee27@users.noreply.github.com>
2023-11-21 15:10:52 +0900

    Update automaticdoor.py
    코드 수정

--- Arduino/automaticdoor.py
+++ Arduino/automaticdoor.py
@@ -3,22 +3,22 @@
 Servo myservo;
 
 int ServoPin = 10; //서보모터 핀
-int trigPin = 13; // 트리그 핀(초음파 센서 송신부)
-int echoPin = 12; // 에코 핀 (초음파 센서 수신부)
+int trig = 13; // 트리그 핀(초음파 센서 송신부)
+int echo = 12; // 에코 핀 (초음파 센서 수신부)
 float duration = 0, distance = 0; //초음파 센서를 통한 주기 및 거리 측정
 int Pos = 0; // 서보 모터 각도 설정
 
 void setup() {
-  pinMode(tirgPin, OUTPUT); 
-  pinMode(echoPin, INPUT);
+  pinMode(trig, OUTPUT); 
+  pinMode(echo, INPUT);
 }
 
 void loop() {
-  digitalwrite(trigPin, HIGH); // 초음파 발생하여 주기 측정
+  digitalWrite(trig, HIGH); // 초음파 발생하여 주기 측정
   delay(10);
-  digitalwrite(trigPin, LOW);	
+  digitalWrite(trig, LOW);	
   
-  duration = pulseln(echoPim, HIGH);// pulseIn() 함수를 이용한 초음파 센서 주기 (ms) 반환
+  duration = pulseIn(echo, HIGH);// pulseIn() 함수를 이용한 초음파 센서 주기 (ms) 반환
   distance = ((float)(340 * duration)) / 10000 / 2; //cm 단위로 거리 변경
   
   if(distance < 15){ //거리가 15cm 미만일떄
@@ -26,13 +26,17 @@ void loop() {
     Pos = 90;
     myservo.write(Pos); // 서보모터 제어
     delay(1000);
+    //Pos = 0;
+    //.write(Pos);
+    //delay(1000);
+    myservo.detach();
+  }
+  else{
+    myservo.attach(ServoPin);
     Pos = 0;
     myservo.write(Pos);
     delay(1000);
     myservo.detach();
-  }
-  else{
-    delay(1000);
   }
 }
 
